8ced41f639558de0fa86e65d2c72cbd92aae93b6
Jen Dela Cruz <purplejen@gmail.com>
2023-10-31 09:48:29 +0800

    Added texts for instructions, button, and terms and conditions

--- mod/quiz/lang/en/quiz.php
+++ mod/quiz/lang/en/quiz.php
@@ -1005,3 +1005,8 @@ $string['yourfinalgradeis'] = 'Your final grade for this quiz is {$a}.';
 $string['numattemptsmade'] = '{$a} attempts made on this quiz';
 $string['reviewofattempt'] = 'Review of attempt {$a}';
 $string['reviewofpreview'] = 'Review of preview';
+
+// Proctor UPOU
+$string['precheckinstructions'] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris augue neque gravida in fermentum. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Cursus in hac habitasse platea dictumst. Nisl vel pretium lectus quam id leo in vitae turpis. Nullam eget felis eget nunc. Diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Ullamcorper velit sed ullamcorper morbi tincidunt. Quis viverra nibh cras pulvinar mattis nunc. Ut etiam sit amet nisl purus in. Pellentesque nec nam aliquam sem et tortor consequat.';
+$string['termsandconditions'] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris augue neque gravida in fermentum. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Cursus in hac habitasse platea dictumst. Nisl vel pretium lectus quam id leo in vitae turpis. Nullam eget felis eget nunc. Diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Ullamcorper velit sed ullamcorper morbi tincidunt. Quis viverra nibh cras pulvinar mattis nunc. Ut etiam sit amet nisl purus in. Pellentesque nec nam aliquam sem et tortor consequat.';
+$string['accepttermsandconditions'] = 'Accept';
